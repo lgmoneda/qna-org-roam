857a119438d057120380626f25ff39704e1081e5
Cumhur Erkut <cumhur.erkut@gmail.com>
2023-06-07 19:32:42 +0200

    Update config.py

--- qna-org-roam/config.py
+++ qna-org-roam/config.py
@@ -1,4 +1,4 @@
 org_roam_path = "/Users/cer/org-roam"
 persist_directory = "/Users/cer/vc/db-org-roam-nodes-openai-qna"
 banned_org_roam_files = {"screening"}
-openai_api_key = "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"
+openai_api_key = "my_api_key"
