e258e8cf6e3146000b4925d5568c1f2c815f4793
Cumhur Erkut <Cumhur.Erkut@gmail.com>
2023-06-07 19:08:49 +0200

    ignores after PR

--- qna-org-roam/config.py
+++ qna-org-roam/config.py
@@ -1,4 +1,4 @@
-org_roam_path = "/paht/to/org/roam/nodes"
-persist_directory = "/path/to/persist/vectordb/db-org-roam-nodes-openai-qna"
+org_roam_path = "/Users/cer/org-roam"
+persist_directory = "/Users/cer/vc/db-org-roam-nodes-openai-qna"
 banned_org_roam_files = {"screening"}
-openai_api_key = "not today pawn!"
+openai_api_key = "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"
